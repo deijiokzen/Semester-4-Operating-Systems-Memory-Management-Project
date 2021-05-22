a75556b56169174c3f8b3a8b23522dd2a3ba7962
deijiokzen <satokoentertainment@gmail.com>
2021-05-22 05:13:07 -0700

    Commitment#3- Figuring out VFS mapping

---
+++ InputList.c
@@ -0,0 +1,160 @@
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include<linux/slab.h>                 //kmalloc()
+#include<linux/uaccess.h>              //copy_to/from_user()
+#include <linux/ioctl.h>
+#include<linux/proc_fs.h>
+
+
+#define LINUX_KERNEL_VERSION  510
+ 
+#define WR_VALUE _IOW('a','a',int32_t*)
+#define RD_VALUE _IOR('a','b',int32_t*)
+ 
+int32_t value = 0;
+char etx_array[20]="try_proc_array\n";
+static int len = 1;
+ 
+ 
+dev_t dev = 0;
+
+static struct proc_dir_entry *parent;
+
+/*
+** Function Prototypes
+*/
+static int      __init etx_driver_init(void);
+static void     __exit etx_driver_exit(void);
+
+/***************** Procfs Functions *******************/
+static int      open_proc(struct inode *inode, struct file *file);
+static int      release_proc(struct inode *inode, struct file *file);
+static ssize_t  read_proc(struct file *filp, char __user *buffer, size_t length,loff_t * offset);
+static ssize_t  write_proc(struct file *filp, const char *buff, size_t len, loff_t * off);
+
+
+#if ( LINUX_KERNEL_VERSION > 505 )
+
+/*
+** procfs operation sturcture
+*/
+static struct proc_ops proc_fops = {
+        .proc_open = open_proc,
+        .proc_read = read_proc,
+        .proc_write = write_proc,
+        .proc_release = release_proc
+};
+
+#else //LINUX_KERNEL_VERSION > 505
+
+/*
+** procfs operation sturcture
+*/
+static struct file_operations proc_fops = {
+        .open = open_proc,
+        .read = read_proc,
+        .write = write_proc,
+        .release = release_proc
+};
+
+#endif //LINUX_KERNEL_VERSION > 505
+
+/*
+** This function will be called when we open the procfs file
+*/
+static int open_proc(struct inode *inode, struct file *file)
+{
+    pr_info("proc file opend.....\t");
+    return 0;
+}
+
+/*
+** This function will be called when we close the procfs file
+*/
+static int release_proc(struct inode *inode, struct file *file)
+{
+    pr_info("proc file released.....\n");
+    return 0;
+}
+
+/*
+** This function will be called when we read the procfs file
+*/
+static ssize_t read_proc(struct file *filp, char __user *buffer, size_t length,loff_t * offset)
+{
+    pr_info("proc file read.....\n");
+    if(len)
+    {
+        len=0;
+    }
+    else
+    {
+        len=1;
+        return 0;
+    }
+    
+    if( copy_to_user(buffer,etx_array,20) )
+    {
+        pr_err("Data Send : Err!\n");
+    }
+ 
+    return length;;
+}
+
+/*
+** This function will be called when we write the procfs file
+*/
+static ssize_t write_proc(struct file *filp, const char *buff, size_t len, loff_t * off)
+{
+    pr_info("proc file wrote.....\n");
+    
+    if( copy_from_user(etx_array,buff,len) )
+    {
+        pr_err("Data Write : Err!\n");
+    }
+    
+    return len;
+}
+
+
+static int __init etx_driver_init(void)
+{
+
+        parent = proc_mkdir("etx",NULL);
+        
+        if( parent == NULL )
+        {
+            pr_info("Error creating proc entry");
+        }
+        
+        /*Creating Proc entry under "/proc/etx/" */
+        proc_create("etx_proc", 0666, parent, &proc_fops);
+ 
+        pr_info("Device Driver Insert...Done!!!\n");
+        return 0;
+ 
+
+}
+ 
+/*
+** Module exit function
+*/
+static void __exit etx_driver_exit(void)
+{
+        /* Removes single proc entry */
+        //remove_proc_entry("etx/etx_proc", parent);
+        
+        /* remove complete /proc/etx */
+        proc_remove(parent);
+        
+        pr_info("Device Driver Remove...Done!!!\n");
+}
+ 
+module_init(etx_driver_init);
+module_exit(etx_driver_exit);
+ 
